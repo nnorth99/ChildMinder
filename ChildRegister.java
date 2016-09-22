b0143416927feb3365f85c28b40bce5711cfa134
nnorth99 <neil.north99@gmail.com>
2016-09-22 16:20:52 +0100

    Update ChildRegister.java

--- ChildRegister.java
+++ ChildRegister.java
@@ -1,54 +1,17 @@
 package childminder;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class ChildRegister
 {
 	static ArrayList<Child> childList = new ArrayList<Child>();
 	static ArrayList<Attendance> attendanceList  = new ArrayList<Attendance>();
-	
+	static ArrayList<RateCard> rateCardList  = new ArrayList<RateCard>();
+
 
 	public static void main( String[] args ) {
-
 		menu();
-
-		int testMode = 1;
-
-		if (testMode ==1) {
-			System.out.println(" *** RUNNING IN TESTING MODE ***");
-			testChild();
-			testAttendance();
-		}
-		else {
-
-			System.out.println("Children - "+childList.size()+" max ID - "+Child.maxId);
-			System.out.println("Attendances - "+attendanceList.size()+" max ID - "+Attendance.maxId);
-
-
-			if(childList.isEmpty()) {
-				System.out.println("No children present");
-			}	    
-			else
-			{System.out.println("We have children present");}
-
-			// save data files away
-			/*      for (int counter = 0; counter < childList.size(); counter++) { 		      
-	    	  System.out.println(childList.get(counter));
-	    	  Child.serializeChild(childList.get(counter));
-	      } 
-			 */	      
-			//			Child.serializeChildList(childList);
-
-			// reset file
-			//			Child.clearChildListFile();
-
-			// add a child
-
-			// check a child in
-
-			// check a child out
-		}
-
 	}
 
 	public static void menu(){
@@ -57,38 +20,56 @@ public class ChildRegister
 
 		while (swValue != 0){
 			// Display menu graphics
-			System.out.println("================================");
-			System.out.println("|     MENU SELECTION DEMO      |");
-			System.out.println("================================");
-			System.out.println("| Options:                     |");
-			System.out.println("|        0. Exit               |");
-			System.out.println("|        1. Load Children      |");
-			System.out.println("|        2. Load Attendances   |");
-			System.out.println("|        3. Load Rates         |");
-			System.out.println("|        4. Add Child          |");
-			System.out.println("|        5. Check in child 1   |");  
-			System.out.println("|        6. Check out child 1  |");
-			System.out.println("|        7. Show Children      |");
-			System.out.println("|        8. Show Attendances   |");
-			System.out.println("|        9. ?                  |");
-			System.out.println("================================");
+			System.out.println("=========================================");
+			System.out.println("|         MENU SELECTION DEMO           |");
+			System.out.println("=========================================");
+			System.out.println("| Options:                              |");
+			System.out.println("|        0. Exit                        |");
+			System.out.println("|        1. Load                        |");
+			System.out.println("|        2. Add Default Rate            |");
+			System.out.println("|        3. Add individual Rate child 1 |");
+			System.out.println("|        4. Add Child                   |");
+			System.out.println("|        5. Check in child 1            |");  
+			System.out.println("|        6. Check out child 1           |");
+			System.out.println("|        7. Show data                   |");
+			System.out.println("|        8.                             |");
+			System.out.println("|        9. Save                        |");
+			System.out.println("=========================================");
 			swValue = KeyInput.inInt(" Select option: ");
 
 			// Switch construct
 			switch (swValue) {
 			case 1:
-				System.out.println("Load Children");
-				childList = initialiseChildren();
+				load();
 				break;
 			case 2:
-				System.out.println("Load Attendances");
-				attendanceList = initialiseAttendance();
+				System.out.println("Add Default Rate");
+
+				LocalDate defaultDate = LocalDate.of(2000, 01, 01);
+				RateCard.createNewRate(3.5f, defaultDate, rateCardList);
+				
+				System.out.println("Number of Rates = "+rateCardList.size());
+				for (RateCard rate: rateCardList){
+					System.out.println(rate);
+				}
+
 				break;
 			case 3:
-				System.out.println("Load Rates - not yet enabled");
+				System.out.println("New Rate for child 1");
+				RateCard.createNewRate(1, 4.5f, LocalDate.now(), rateCardList);
+				
+				for (RateCard rate: rateCardList){
+					System.out.println(rate);
+				}
 				break;
 			case 4:
 				System.out.println("Add Child");
+				// Adding child
+				Child newChild = new Child();
+				newChild.setChild(++Child.maxId, "New", "Child "+Child.maxId);
+				childList.add(newChild);
+
+				System.out.println("size of array : "+ childList.size());
 				break;
 			case 5:
 				System.out.println("Check in child 1");
@@ -97,7 +78,7 @@ public class ChildRegister
 					System.out.println("no children exist, so can't check in child 1");
 				}
 				else {
-				childList.get(0).checkIn();
+					childList.get(0).checkIn();
 				}
 				break;
 			case 6:
@@ -106,27 +87,29 @@ public class ChildRegister
 					System.out.println("no children exist, so can't check out child 1");
 				}
 				else {
-				childList.get(0).checkOut();
+					childList.get(0).checkOut();
 				}
 				break;
 			case 7:
-				System.out.println("Show Children");
 				System.out.println("Number of Children = "+childList.size());
 				for (Child kid : childList){
 					System.out.println(kid);
 				}
-				break;
-			case 8:
-				System.out.println("Show Attendances");
 				System.out.println("Number of Attendances = "+attendanceList.size());
 				for (Attendance att : attendanceList){
 					System.out.println(att);
 				}
+				System.out.println("Number of Rate Cards = "+rateCardList.size());
+				for (RateCard rate : rateCardList){
+					System.out.println(rate);
+				}
+				break;
+			case 8:
+				System.out.println("Option not yet enabled");
 				break;
 			case 9:
 				System.out.println("Save");
-				Child.serializeChildList(childList);
-				Attendance.serializeAttendanceList(attendanceList);
+				save();
 				break;
 			case 0:
 				System.out.println("Exit selected");
@@ -135,6 +118,39 @@ public class ChildRegister
 				System.out.println("Invalid selection");
 				break; // This break is not really necessary
 			}}
+	}
+
+	public static void save (){
+		// serialise
+		System.out.println("save children - elements = "+ childList.size());
+		Child.serializeChildList(childList);
+		System.out.println("save attendances - elements = "+ attendanceList.size());
+		Attendance.serializeAttendanceList(attendanceList);
+		System.out.println("save Rates - elements = "+ rateCardList.size());
+		RateCard.serializeRateCardList(rateCardList);
+	}
+
+	public static void load (){
+		// deserialise
+		System.out.println("Load Children");
+		childList = initialiseChildren();
+		System.out.println("Load Attendances");
+		attendanceList = initialiseAttendance();
+		System.out.println("Load Rates");
+		rateCardList = initialiseRates();
+	}
+
+
+	public static ArrayList<RateCard> initialiseRates (){
+		// deserialise
+		ArrayList<RateCard> totallyNewList = RateCard.deserializeRateCardList();
+		System.out.println("initialiseRates");
+		for (int counter = 0; counter < totallyNewList.size(); counter++) { 		      
+			System.out.println("counter : "+counter);
+			System.out.println(totallyNewList.get(counter));
+		} 
+		Child.maxId = totallyNewList.size();
+		return totallyNewList;
 	}
 
 	public static ArrayList<Child> initialiseChildren (){
@@ -147,10 +163,8 @@ public class ChildRegister
 		} 
 		Child.maxId = totallyNewList.size();
 		return totallyNewList;
-
-
-		// set max ID code
 	}
+
 	public static ArrayList<Attendance> initialiseAttendance (){
 		// deserialise
 		ArrayList<Attendance> totallyNewList = Attendance.deserializeAttendanceList();
@@ -161,54 +175,6 @@ public class ChildRegister
 		} 
 		Attendance.maxId = totallyNewList.size();
 		return totallyNewList;
-
-
-		// set max ID code
 	}
-
-	private static void testChild(){
-		System.out.println("testing Child processing");
-
-		System.out.println("getting Children from file:");
-		ArrayList<Child> testChildArray = initialiseChildren();
-		System.out.println("Array now holds "+testChildArray.size());
-
-		// trial of adding children method #1
-		Child testChild = new Child();
-		testChild.setChildID(91);
-		testChild.setFirstName("Test");
-		testChild.setLastName("Child");
-		testChildArray.add(testChild);
-
-
-		// Adding children - method #2
-		Child testChild2 = new Child();
-		testChild2.setChild(92, "Child-Two", "Test");
-		testChildArray.add(testChild2);
-
-		System.out.println("size of array : "+ testChildArray.size());
-
-
-
-	}
-
-	private static void testAttendance(){
-		System.out.println("testing Attendance processing");
-
-		System.out.println("getting Attendances from file:");
-		ArrayList<Attendance> testAttendanceArray = initialiseAttendance();
-		System.out.println("Array now holds "+testAttendanceArray.size());
-
-		// trial adding attendances -  checking child in
-
-		// trial adding attendances -  checking child out default
-
-		// trial adding attendances -  checking child in
-
-
-	}
-
-
-
 
 }
